273789e13adc1c90b4f0c2061a635257ff9d44ef
AthanasiaPet <athanasia.petsa@yahoo.com>
2026-01-18 22:40:42 +0200

    feat: add product removal with confirmation

--- cart.php
+++ cart.php
@@ -71,25 +71,38 @@ if (!empty($cart)) {
                 <tr>
                     <td><?php echo htmlspecialchars($product['name']); ?></td>
                     <td>€<?php echo number_format($product['price'], 2); ?></td>
-                    <td> <div class="qty-controls">
-        <a href="update_cart.php?id=<?php echo $product['id']; ?>&action=decrease" class="qty-btn">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                 stroke-width="1.5" stroke="currentColor" class="qty-icon">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M5 12h14" />
-            </svg>
-        </a>
+                    <td>
+             <div class="qty-controls">
+            
+                    <!-- plus button -->
+                    <a href="update_cart.php?id=<?php echo $product['id']; ?>&action=decrease" class="qty-btn"> 
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                         stroke-width="1.5" stroke="currentColor" class="qty-icon">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M5 12h14" />
+                        </svg>
+                     </a>
 
-        <span class="qty-number"><?php echo $qty; ?></span>
+                     <span class="qty-number"><?php echo $qty; ?></span>
+                    
+                     <!-- minus button -->
+                    <a href="update_cart.php?id=<?php echo $product['id']; ?>&action=increase" class="qty-btn">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                        stroke-width="1.5" stroke="currentColor" class="qty-icon">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M12 4.5v15m7.5-7.5h-15" />
+                        </svg>
+                    </a>
 
-        <a href="update_cart.php?id=<?php echo $product['id']; ?>&action=increase" class="qty-btn">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                 stroke-width="1.5" stroke="currentColor" class="qty-icon">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                      d="M12 4.5v15m7.5-7.5h-15" />
-            </svg>
-        </a>
-    </div>
-                    </td>
+                    <!-- delete button -->
+                    <a href="update_cart.php?id=<?php echo $product['id']; ?>&action=remove" class="qty-btn delete-btn"  onclick="return confirm('Να αφαιρεθεί το προϊόν από το καλάθι;')">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                         stroke-width="1.5" stroke="currentColor" class="qty-icon delete-btn">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                        </svg>
+
+                    </a>
+            </div>
+                </td>
                     <td>€<?php echo number_format($sum, 2); ?></td>
                 </tr>
                 <?php endforeach; ?>
@@ -108,6 +121,7 @@ if (!empty($cart)) {
 <footer>
     <p>&copy; 2026 Cat Shop</p>
 </footer>
+
 
 </body>
 </html>
