203e8bb04d4868e87d15ec46e3ddc109fca9988f
AthanasiaPet <athanasia.petsa@yahoo.com>
2026-01-21 17:00:39 +0200

    feat: add successful  order page

---
+++ success.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="el">
+<head>
+    <meta charset="UTF-8">
+    <title>Ολοκλήρωση Αγοράς - Cat Shop</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/success.css">
+    
+</head>
+<body>
+
+<header>
+    <h1><a href="index.php" class="logo">Cat Shop</a></h1>
+    <nav>
+        <a href="shop.php" class="cart-icon">Καλάθι</a>
+    </nav>
+</header>
+
+<main class="success-page">
+    <div class="success-card">
+        <h2>Η αγορά ολοκληρώθηκε επιτυχώς.</h2>
+        <p>Σας ευχαριστούμε για την προτίμηση! Σύντομα θα λάβετε e-mail με την εξέλιξη της παραγγελίας.</p>
+        <a href="shop.php">Συνέχεια αγορών</a>
+    </div>
+</main>
+
+<footer>
+    <p>&copy; 2026 Cat Shop</p>
+</footer>
+
+</body>
+</html>
